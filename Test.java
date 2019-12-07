2016b08f644533ef92b32ac4cc0386849f57de65
Bernadetta Swidrak <bernadetta.swidrak@gmail.com>
2019-12-07 12:04:24 +0000

    Added main file

---
+++ Test.java
@@ -0,0 +1,19 @@
+public class Test {
+    public static void main(String [] args) {
+        for (String s : args) {
+
+
+            System.out.println(s);
+            System.out.print (s);
+            System.out.print (s);
+            System.out.println(s);
+            System.out.println(s);
+            System.out.println(s);
+            System.out.println(s);
+
+
+        }
+    }
+}
+
+
